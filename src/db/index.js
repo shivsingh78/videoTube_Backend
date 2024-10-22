41fe07f325c0fcb9e9ce44b19362f47be70ab660
shivsingh78 <shivsinghbaghel95@gmail.com>
2024-10-23 01:43:13 +0530

    connect to mongo ATLAS

---
+++ src/db/index.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+ import { DB_NAME } from "../constants.js";
+ // Import the DB_NAME from constants.js
+
+ const connectDB = async () =>{
+     try{
+
+          console.log(process.env.MONGODB_URI)
+          // Check if the environment variable is set
+          // Connect to MongoDB
+          const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+          console.log(`\n MongoDB connected !! DB HOST:  ${connectionInstance.connection.host} `);
+          // If connection fails, throw an error
+
+
+
+     } catch (error){
+          console.log("MONGODB connection Fail ", error);
+          process.exit(1)
+          
+          // If connection fails, throw an error
+     }
+ }
+ // Export the function to be used in other files
+
+ export default connectDB
